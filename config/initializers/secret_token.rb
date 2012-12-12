1d503d078b471c9ac97fc76706879b4045f2f723
Guy Archibald <goarchib83@gmail.com>
2012-12-12 12:39:55 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DealersRevenge::Application.config.secret_token = 'de7a8c2f5f57b6f16e0b4ff8299da31e138c4662bfd251006920a653a249f204faea91dac957cf525f083431e1d42b8f928fb27e22222419a97f9d676a2b7c85'
