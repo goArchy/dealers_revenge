cd076afe394ab83a6df4fbe1301c0e89386becb1
Guy Archibald <goarchib83@gmail.com>
2012-12-17 16:37:05 -0600

    Add require faker to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+require 'faker'
+
 Admin.create!(email: "admin@example.org",
              password: "foobar",
              password_confirmation: "foobar")
